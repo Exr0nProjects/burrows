e967adf5ba9cec772b65443cddd424b364ad3e3f
Exr0n <howdy@exr0n.com>
2023-08-23 03:15:28 -0700

    organize audiocap code into pipeline blocks

--- audiocap/src/worker.ts
+++ audiocap/src/worker.ts
@@ -38,79 +38,60 @@ Break the transcript into bullet points, exactly preserving the structure and wo
 
 export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-        const req_formdata = await request.formData();
 
+        const req_formdata = await request.formData();
         const keys = [ 'location', 'loc-long', 'loc-lat', 'loc-alt' ]
         const req_metadata = Object.fromEntries(keys.map(k => [k, req_formdata.get(k)]))
 
-        const transcription_fd = new FormData();
-        const audio_file = (req_formdata.get('file')! as unknown as File);
-        const audio_as_blob = new Blob([audio_file], { type: audio_file.type });
-        transcription_fd.append('file', audio_as_blob);
-        transcription_fd.append('model', 'whisper-1');
+        // transcribe audio
+        const [ transcript_res, audio_filesize ] = await (async () => {
+            const transcription_fd = new FormData();
+            const audio_file = (req_formdata.get('file')! as unknown as File);
+            const audio_as_blob = new Blob([audio_file], { type: audio_file.type });
+            transcription_fd.append('file', audio_as_blob);
+            transcription_fd.append('model', 'whisper-1');
 
-        const whisperResponse = await fetch('https://api.openai.com/v1/audio/transcriptions', {
-            method: 'POST',
-            headers: {
-              'Authorization': `Bearer ${env.OPENAI_API_KEY}`,
-            },
-            body: transcription_fd
-        })
-        // optm: give it a list of names or toggl project names?
-        const transcript_res: string = (await whisperResponse.json() as { text: string }).text;
-
-
-
-
-        const configuration = new Configuration({
-            apiKey: env.OPENAI_API_KEY,
-            baseOptions: {
-                adapter: fetchAdapter
-            }
-        });
-        const openai = new OpenAIApi(configuration);
-        const completion = await openai.createChatCompletion({
-            // messages: [{ role: "system", content: "please say hello" }],
-            messages: [
-                { role: "system", content: postprocessing_prompt },
-                { role: "user", content: transcript_res }
-            ],
-            model: "gpt-3.5-turbo",
-        });
-
-        const msg = completion.data
+            const whisperResponse = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+                method: 'POST',
+                headers: {
+                'Authorization': `Bearer ${env.OPENAI_API_KEY}`,
+                },
+                body: transcription_fd
+            })
+            // optm: give it a list of names or toggl project names?
+            return [ (await whisperResponse.json() as { text: string }).text, audio_as_blob.size ]
+        })();
 
 
-
-
-        // const openai = new OpenAIApi(configuration);
-        // const chatCompletion = await openai.createChatCompletion({
-        //     model: "gpt-3.5-turbo-0613",
-        //     messages: [
-        //         // { role: "system", content: postprocessing_prompt },
-        //         // { role: "user", content: transcript_res }
-        //         { role: "user", content: "whats up with charles dickenson?" }
-        //     ],
-        //     functions: [ ]
-        // });
-
-        // const msg = chatCompletion.data.choices[0].message!;
-        // const msg = 'hi'
-
-
-
-
-
+        // organize transcript
+        const postprocess_res = await ( async() => {
+            const configuration = new Configuration({
+                apiKey: env.OPENAI_API_KEY,
+                baseOptions: {
+                    adapter: fetchAdapter
+                }
+            });
+            const openai = new OpenAIApi(configuration);
+            const completion = await openai.createChatCompletion({
+                // messages: [{ role: "system", content: "please say hello" }],
+                messages: [
+                    { role: "system", content: postprocessing_prompt },
+                    { role: "user", content: transcript_res }
+                ],
+                model: "gpt-3.5-turbo",
+            });
+            return completion.data;
+        })();
 
 
         // Return the response
         const res = {
             input_stats: {
-                filesize_kb: audio_as_blob.size/1024,
+                filesize_kb: audio_filesize/1024,
                 metadata: req_metadata,
             },
             raw_transcript: transcript_res,
-            postprocess_msg: msg
+            postprocess_msg: postprocess_res
         }
         return new Response(JSON.stringify(res, null, 4), { status: 200 })
     },
