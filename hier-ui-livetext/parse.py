d590a806ae2ef6508c6ffe302be7f47799c7e303
Exr0n <howdy@exr0n.com>
2023-11-15 17:43:00 -0800

    numbered boxes + naive pixel-on-edge filtering

--- hier-ui-livetext/parse.py
+++ hier-ui-livetext/parse.py
@@ -16,7 +16,7 @@ console = Console()
 
 @dataclass
 class Box:
-    p: List[Tuple[float, float]]
+    p: List[Tuple[float, float]]    # top left, top right, bottom left, bottom right
     t: str
 
 def get_richText_points(path: str) -> List[Box]:
@@ -41,20 +41,42 @@ class Filters:
             #     or box.p[2][1] - box.p[3][1] > tol
         return np.array([is_square(b) for b in boxes])
 
+    @classmethod
+    def away_from_border(cls, boxes: List[Box]):
+        def is_border(box):
+            if box.p[3][0] > 0.95:
+                print(box.p[3][0])
+            return any(p[0] == 0 or p[1] == 0 or p[0] == 1 or p[1] == 1 for p in box.p)
+        return np.array([is_border(b) for b in boxes])
+
 def main_annotate(img, boxes):
-    color = '#326ccc'
     thickness = 1
+    # font variables from https://stackoverflow.com/a/34273603
+    font      = cv2.FONT_HERSHEY_SIMPLEX
+    lineType  = 2
+
 
     img = img.copy()
     coordify = lambda p: (int(p[0]*img.shape[1]), img.shape[0]-int(p[1]*img.shape[0]))
 
+    filters = [
+        Filters.alignment_is_square,
+        Filters.away_from_border
+    ]
+    mask = np.any([f(boxes) for f in filters], axis=0)
+    print(mask)
+
+    color = '#326ccc'
     colors = np.array([color]*len(boxes))
-    colors = np.ma.array(colors, mask=Filters.alignment_is_square(boxes)).filled('#ff0000')
+    colors = np.ma.array(colors, mask=mask).filled('#ff0000')
+    colors = [hex_to_rgb(c) for c in colors]
 
 
-    for box, color in zip(boxes, colors):
+    for i, (box, color) in enumerate(zip(boxes, colors)):
         for i1, i2 in [(0, 1), (1, 3), (0, 2), (2, 3)]:
-            cv2.line(img, coordify(box.p[i1]), coordify(box.p[i2]), hex_to_rgb(color), thickness, lineType=cv2.LINE_AA)
+            cv2.line(img, coordify(box.p[i1]), coordify(box.p[i2]), color, thickness, lineType=cv2.LINE_AA)
+
+        cv2.putText(img, f"{i}", coordify(box.p[2]), font, 0.02 * abs(coordify(box.p[0])[1] - coordify(box.p[2])[1]), color, 3, lineType)
 
     cv2.imshow('pictoor', img)
     cv2.waitKey(0)
---
+++ hier-ui-livetext/readme.md
@@ -0,0 +1,3 @@
+possible edgecases/heuristics
+- vertical and horizontal space jumps: tabs and newlines. check that it's all one solid color?
+- distances
