aa3145ad4389a7ca6004e6b2a9f55f350d33d08c
Exr0n <howdy@exr0n.com>
2023-11-20 16:56:24 -0800

    color scatters; hard to get stuff out it seems

--- hier-ui-livetext/parse.py
+++ hier-ui-livetext/parse.py
@@ -8,6 +8,8 @@ from subprocess import run
 import numpy as np
 from operator import itemgetter as ig, attrgetter as ag, methodcaller as mc
 from functools import partial, cached_property
+from matplotlib import pyplot as plt
+from macropy.quick_lambda import macros, f, _
 
 from util import *
 
@@ -28,7 +30,10 @@ class Box:
     @classmethod
     def from_pt(cls, img, p, t):
         (x1, y1), (x2, y2) = coordify(img, p[0]), coordify(img, p[3])
-        return Box(p, t, img[x1:x2, y1:y2], img.shape[:2])
+        (x3, y3), (x4, y4) = coordify(img, p[0]), coordify(img, p[3])
+        x1, x2 = min(x1, x2, x3, x4), max(x1, x2, x3, x4)
+        y1, y2 = min(y1, y2, y3, y4), max(y1, y2, y3, y4)
+        return Box(p, t, img[y1:y2, x1:x2], img.shape[:2])
 
     @classmethod
     def from_corners(cls, img: np.ndarray, p1, p2, t):
@@ -37,13 +42,33 @@ class Box:
     @cached_property
     def fg_bg_colors(self) -> Tuple[List[Tuple[int, int, int]], List[Tuple[int, int, int]]]:
         """returns in BGR to match opencv"""
-        n_samples = max(self.w_px * self.h_px//10, 50)
+        n_samples = 100
+        # n_samples = max(self.w_px * self.h_px//10, 50)    # TODO: put back
         rng = np.random.default_rng()
-        print(self.i, np.vstack(self.i).shape, 'cow')
+        # print(self.i, np.vstack(self.i).shape, 'cow')
         sample = rng.choice(np.vstack(self.i), size=n_samples)  # TODO: from last time. was making a random-sample thing and then ig just histogram/counting sort to find the bg color and fg color? or could do max distance of unifrom color or smt, bc fonts are usually stroke-based while the area between is usually larger
+
+        # colors = [bgr_to_hex(pix) for pix in sample]
+        # sample = np.apply_along_axis(bgr_to_hsv, 1, sample)
+        # fig = plt.figure()
+        # ax = fig.add_subplot(211, projection='3d')
+        # img_ax = fig.add_subplot(212)
+        # img_ax.imshow(self.i)
+        # img_ax.set_title(self.t)
+        # ax.scatter(*sample.T, c=colors)
+        # ax.set_xlabel('v')
+        # ax.set_ylabel('s')
+        # ax.set_zlabel('h')
+        # plt.show()
+        # print(sample)
+
         fgs = []
         bgs = []
         return [], []
+
+    @cached_property
+    def est_font_size(self) -> float:
+        return self.h
 
     @property
     def tl(self): return self.p[0]
@@ -119,14 +144,13 @@ def draw_boxes(img, boxes, colors, labels=None):
     if type(labels) is str: labels = [labels] * len(boxes)
     else: assert len(labels) == len(boxes)
 
-    print(colors, type(colors))
     if type(colors) is str: colors = [colors] * len(boxes)
     else: assert len(colors) == len(boxes)
 
 
     cify = partial(coordify, img)
     for i, (box, color) in enumerate(zip(boxes, colors)):
-        color = hex_to_rgb(color) if type(color) is str else color
+        color = hex_to_bgr(color) if type(color) is str else color
 
         for i1, i2 in [(0, 1), (1, 3), (0, 2), (2, 3)]:
             cv2.line(img, cify(box.p[i1]), cify(box.p[i2]), color, thickness, lineType=cv2.LINE_AA)
@@ -136,15 +160,12 @@ def draw_boxes(img, boxes, colors, labels=None):
 
         cv2.putText(img, f"{i}", cify(box.p[2]), font, 0.02 * abs(cify(box.p[0])[1] - cify(box.p[2])[1]), color, 3, lineType)
 
-        # print(box.fg_bg_colors)
-
 def filter_boxes_with(boxes, *filters, img: np.ndarray=None, color: str=None, label: str=None) -> List[Box]:
     mask = np.all([f(boxes) for f in filters], axis=0)
     masked_out = np.ma.array(boxes, mask=mask).compressed()
     if img is not None:
         if color is None: color = '#ff0000'
         if label is None: label = 'masked out'
-        print(color, label)
         draw_boxes(img, masked_out, color, label)
     return np.ma.array(boxes, mask=~mask).compressed()
 
@@ -183,15 +204,20 @@ def main_annotate(img, boxes):
 
     # draw_boxes(img, boxes, '#007700', 'correct')
 
+    fig, ax = plt.subplots()
+    ax.hist([b.est_font_size for b in boxes], bins=100)
+    plt.show()
+
     cv2.imshow('pictoor', img)
     cv2.waitKey(0)
 
 def main():
     # points: List[Box] = loads(argv[1])
-    # console.print(points)
 
     impath = argv[1]
     boxes = get_richText_points(impath)
+    for box in boxes:
+        print(box.fg_bg_colors)
     img = read_img(impath)
     main_annotate(img, boxes)
 
--- hier-ui-livetext/util.py
+++ hier-ui-livetext/util.py
@@ -1,5 +1,14 @@
-def hex_to_rgb(hex_color: str):
+import numpy as np
+from colorsys import rgb_to_hsv, hsv_to_rgb
+
+def hex_to_bgr(hex_color: str):
     hex_color = hex_color.strip()
     if hex_color.startswith('#'): hex_color = hex_color[1:]
     assert len(hex_color) == 6
     return list(reversed([int(hex_color[i:i+2], base=16) for i in range(0, 5, 2)]))
+
+def bgr_to_hex(bgr: np.ndarray):
+    return '#' + ''.join(f"{n:02x}" for n in reversed(bgr))
+
+def bgr_to_hsv(bgr: np.ndarray):
+    return rgb_to_hsv(*reversed(bgr))
